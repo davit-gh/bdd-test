24c823ee9d20d18b52e61cead3d5c69ca595d6e2
david-bb <kartashyan.davit@gmail.com>
2019-04-18 13:35:49 +0400

    Run Chrome in headless mode

--- webdriver.py
+++ webdriver.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 class Driver:
     def __init__(self):
-        self.instance = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.instance = webdriver.Chrome(options=chrome_options)
         self.instance.maximize_window()
-
     def navigate(self, url):
         if isinstance(url, str):
             self.instance.get(url)
